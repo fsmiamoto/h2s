14e951fbb33be79974c8b4ae71122f76bbb267f0
Francisco Miamoto <francisco@dorayaki.co>
2021-10-11 21:27:10 -0300

    feat: add regex to match lines

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::collections::HashMap;
+extern crate skim;
+use skim::prelude::*;
+use std::io::Cursor;
 use structopt::StructOpt;
+use regex::Regex;
 
 #[derive(StructOpt)]
 #[structopt(name = "http2shell", about = "shell2http back to the shell!")]
@@ -15,12 +18,45 @@ struct App {
     params: Option<String>,
 }
 
+fn do_request(url: &str) -> Result<reqwest::blocking::Response, reqwest::Error> {
+    reqwest::blocking::get(url)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = App::from_args();
 
     println!("{} - {:?} - {:?}", args.hostname, args.cmd, args.params);
 
-    let body = reqwest::blocking::get(args.hostname)?.text()?;
-    println!("{:#?}", body);
+    let body = do_request(&args.hostname).unwrap().text().unwrap();
+
+    let re2 = Regex::new(r"\s*<li><a[^>]*>/([^<]+)</a> <span[^>]*>- ([^>]+)<span>").unwrap();
+
+    let mut list = String::new();
+
+    // TODO: Add proper error handling
+    for m in re2.captures_iter(&body) {
+        let name = m.get(1).unwrap().as_str();
+        let cmd = m.get(2).unwrap().as_str();
+        list.push_str(format!("{}: {}\n", name, cmd).as_str());
+    }
+
+    let options = SkimOptionsBuilder::default()
+        .height(Some("100%"))
+        .multi(false)
+        .build()
+        .unwrap();
+
+    let item_reader = SkimItemReader::default();
+    let items = item_reader.of_bufread(Cursor::new(list));
+
+    // `run_with` would read and show items from the stream
+    let selected_items = Skim::run_with(&options, Some(items))
+        .map(|out| out.selected_items)
+        .expect("no selected option");
+
+    for item in selected_items.iter() {
+        print!("{}{}", item.output(), "\n");
+    }
+
     Ok(())
 }
